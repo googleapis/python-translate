622caf4703b14964652c7e2811d582e4a1e6a341
Leah Cole <coleleah@google.com>
2020-01-31 11:24:08 -0800

    fix: more test_utils

--- synth.py
+++ synth.py
@@ -79,13 +79,12 @@ s.replace(
 # ----------------------------------------------------------------------------
 # Add templated files
 # ----------------------------------------------------------------------------
-templated_files = gcp.CommonTemplates().py_library(unit_cov_level=100, cov_level=100)
-s.move(templated_files)
-
-s.shell.run(["nox", "-s", "blacken"], hide_output=False)
-
+# templated_files = gcp.CommonTemplates().py_library(unit_cov_level=100, cov_level=100)
 # Pass dependencies to system tests
 templated_files = common.py_library(
     cov_level=79,
     system_test_dependencies=['test_utils']
 )
+s.move(templated_files)
+
+s.shell.run(["nox", "-s", "blacken"], hide_output=False)
