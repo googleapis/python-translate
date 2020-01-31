6d68ad66491eac22faf90386db8af73429148bdb
Leah Cole <coleleah@google.com>
2020-01-31 13:02:58 -0800

    fix: add bu sun suggestions

--- synth.py
+++ synth.py
@@ -82,7 +82,7 @@ s.replace(
 # templated_files = gcp.CommonTemplates().py_library(unit_cov_level=100, cov_level=100)
 # Pass dependencies to system tests
 templated_files = common.py_library(
-    unit_cov_level=79, cov_level=79,
+    unit_cov_level=95, cov_level=95,
     system_test_dependencies=['test_utils']
 )
 s.move(templated_files)
